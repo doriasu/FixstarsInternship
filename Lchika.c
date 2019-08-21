fc5da748d9a44603024b38bbc24202346d06b185
takuro.omori <takuro.omori@fixstars.com>
2019-08-21 10:38:22 +0900

    atoiをstrtoulに修正

--- Lchika.c
+++ Lchika.c
@@ -1,8 +1,10 @@
 #include <arm/am335x.h>
+#include <errno.h>
 #include <hw/inout.h>
 #include <stdint.h>
 #include <stdio.h>
-#include<stdlib.h>
+#include<limits.h>
+#include <stdlib.h>
 #include <sys/mman.h>
 #define LED0 (1 << 21)
 #define LED1 (1 << 22)
@@ -15,17 +17,18 @@ int main(int argc, char **argv) {
     return 0;
   }
   char *input = argv[1];
-  int tmp=atoi(input);
-  int bit=0;
-  int base=1;
-  //2進数を10進数変換
-  while(tmp>0){
-    bit = bit + ( tmp % 10 ) * base;
-    tmp = tmp / 10;
-    base = base * 2;
+  char *endptr;
+  errno = 0;
+  unsigned long bit = strtoul(input, &endptr, 2);
+  if (*endptr != '\0') {
+    perror(
+        "不正な文字が入力されています。引数は4ケタの2進数のみにしてください\n");
+    return 0;
   }
-
-
+  if (bit == LONG_MAX && errno == ERANGE) {
+    perror("値が大きすぎます。");
+    return 0;
+  }
 
   // 0x4804C000から0x00001000文の領域アクセス権利を取得
   const uintptr_t gpio_base =
@@ -36,9 +39,7 @@ int main(int argc, char **argv) {
 
   const uint32_t data_value =
       in32(gpio_base + GPIO_DATAOUT) & ~(LED0 | LED1 | LED2 | LED3);
-  out32(gpio_base + GPIO_DATAOUT, data_value |LEDS(bit) );
-  
-  
+  out32(gpio_base + GPIO_DATAOUT, data_value | LEDS(bit));
 
   munmap_device_io(gpio_base, AM335X_GPIO_SIZE);
 
